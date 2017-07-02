ccb97cab2de3e51d6370790ce8eaab871caad525
Ippei Nawate <saku.ni63@gmail.com>
2017-07-03 08:34:54 +0900

    That's fun!!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-/**
- * Created by sakuna63 on 2017/03/15.
- */
+import com.sun.management.jmx.Trace.send
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.produce
+import java.io.Closeable
+import kotlin.coroutines.experimental.CoroutineContext
 
-fun main(args: Array<String>) {
-    println("test")
+fun main(args: Array<String>) = runBlocking {
+}
+
+fun filter(context: CoroutineContext, numbers: ReceiveChannel<Int>, prime: Int) = produce<Int>(context) {
+    for (x in numbers) if (x % prime != 0) send(x)
+}
+
+fun numbersFrom(context: CoroutineContext, start: Int) = produce<Int>(context) {
+    var x = start
+    while (true) send(x++) // infinite stream of integers from start
 }
